a570d88f45c94f6f44df75db17a2d50a775819c2
helalha7 <helal.hammoud82@gmail.com>
2025-06-02 18:16:47 +0300

    Refactored auth page and fixed Suspense issue

--- src/app/auth/page.js
+++ src/app/auth/page.js
@@ -1,8 +1,7 @@
 'use client';
 
 import { Suspense } from 'react';
-import AuthPage from '@/components/pages/AuthPage';
-
+import AuthPage from '@/components/auth/AuthPage'
 export default function AuthRoute() {
     return (
         <Suspense fallback={<div className="text-center mt-20 text-gray-500">Loading authentication...</div>}>
