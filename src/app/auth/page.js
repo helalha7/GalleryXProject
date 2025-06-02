0d892bbb5daa0bca014b71d95f727f32b755969e
helalha7 <helal.hammoud82@gmail.com>
2025-06-02 18:27:08 +0300

    vercel redeploy

--- src/app/auth/page.js
+++ src/app/auth/page.js
@@ -1,7 +1,7 @@
 'use client';
 
 import { Suspense } from 'react';
-import AuthPage from '@/components/auth/AuthPagde'
+import AuthPage from '@/components/auth/AuthPage'
 export default function AuthRoute() {
     return (
         <Suspense fallback={<div className="text-center mt-20 text-gray-500">Loading authentication...</div>}>
